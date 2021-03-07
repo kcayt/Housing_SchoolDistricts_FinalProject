0834b9f40462ffa5a81baff606eaddfcd2e03720
kcayt <kcayt@users.noreply.github.com>
2021-03-07 10:50:47 -0500

    REVISED GeoJSON Map - FARinSD25+28
     FAR in SDs 25+28

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,71 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2NheXQiLCJhIjoiY2tsZWY0cGJmMWRtZjJucXRqdGJhdnJ2OSJ9.5yR0jnpb7zMjnGWCj_Jr-g';
 // function to translate NYC land use codes into a color and a description
+
 var FAR = (code) => {
   switch (code) {
     case 1:
       return {
-        color: 'pink',
+        // color: '#ccc',
         description: 'Max Residential FAR = 10',
       };
     case 2:
       return {
-        color: 'red',
+        // color: 'red',
         description: 'Max Residential FAR = 7.52',
       };
     case 3:
       return {
-        color: '#FF9900',
+        // color: '#FF9900',
         description: 'Max Residential FAR = 6.02',
       };
     case 4:
       return {
-        color: '#f7cabf',
+        // color: '#f7cabf',
         description: 'Max Residential FAR = 5',
       };
     case 5:
       return {
-        color: '#ea6661',
+        // color: '#ea6661',
         description: 'Max Residential FAR = 4',
       };
     case 6:
       return {
-        color: '#d36ff4',
+        // color: '#d36ff4',
         description: 'Max Residential FAR = 3.44',
       };
       case 7:
         return {
-          color: 'tan',
+          // color: 'tan',
           description: 'Max Residential FAR = 3',
         };
          case 8:
             return {
-              color: 'white',
+              // color: 'white',
               description: 'Max Residential FAR = 2.43',
             };
             case 9:
                 return {
-                  color: 'yellow',
+                  // color: 'yellow',
                   description: 'Max Residential FAR = 2',
                 };
                 case 10:
                     return {
-                      color: 'yellow',
+                      // color: 'yellow',
                       description: 'Max Residential FAR = 1.25',
                     };
                     case 11:
                         return {
-                          color: 'yellow',
+                          // color: 'yellow',
                           description: 'Max Residential FAR = 0.9',
                         };
                         case 12:
                             return {
-                              color: 'yellow',
+                              // color: 'yellow',
                               description: 'Max Residential FAR = 0.75',
                             };
                             case 13:
                                 return {
-                                  color: 'yellow',
+                                  // color: 'yellow',
                                   description: 'Max Residential FAR = 0.5',
                                 };
       }
@@ -82,76 +83,40 @@ zoom: 11.5 // starting zoom
      map.addSource('gismap', {
         type: 'geojson',
         data: 'data/gismap.geojson'
-      });
 
-  map.addLayer({
-  'id': 'fill-layer',
-  'type': 'fill',
-  'source': 'gismap',
-  'layout': {},
-  'paint': {
-  'fill-color': {
-        type: 'categorical',
+ });
+
+    map.addLayer({
+      'id': 'fill-layer',
+      'type': 'fill',
+      'source': 'gismap',
+      'layout': {},
+      'paint': {
+      'fill-color': {
         property: 'ResidFAR',
         stops: [
-          [
-            '10',
-            FAR(1).color,
-          ],
-          [
-            '7.52',
-            FAR(2).color,
-          ],
-            [
-              '6.02',
-            FAR(3).color,
-            ],
-            [
-              '5',
-            FAR(4).color,
-            ],
-            [
-              '4',
-            FAR(5).color,
-              ],
-            [
-              '3.44',
+          [0.5,'white',],
+          [0.75,'white',],
+          [0.9,'grey',],
+          [1.25,'grey',],
+          [2,'#df65b0',],
+          [2.43,'#e7298a',],
+            [3, '#d4b9da',],
+            [3.44, '#c994c7',],
+            [4, '#df65b0',],
+            [5, '#e7298a' ,],
+            [6.02, '#ce1256',],
+            [7.52,'#980043',],
+            [10, '#67001f',]
 
-            FAR(6).color,
-              ],
-            [
-              '3',
-              FAR(7).color,
-            ],
-            [
-              '2.43',
-              FAR(8).color,
-            ],
-            [
-              '2',
-              FAR(9).color,
-            ],
-            [
-              '1.25',
-              FAR(10).color,
-            ],
-            [
-              '0.9',
-              FAR(11).color,
-            ],
-            [
-              '0.75',
-              FAR(12).color,
-            ],
-            [
-              '0.5',
-              FAR(13).color,
-            ],
-          ]},
-            'fill-outline-color': '#ccc',
-            'fill-opacity': 0.8
-          }
-        });
+        ]
+      }
+      ,
+          'fill-outline-color': 'grey',
+          'fill-opacity': 0.8
+        }
+      });
+
 
         // add an empty data source, which we will use to highlight the lot the user is hovering over
           map.addSource('highlight-feature', {
@@ -195,13 +160,13 @@ zoom: 11.5 // starting zoom
             var hoveredFeature = features[0]
             var address = hoveredFeature.properties.ZoneDist1
               var built = hoveredFeature.properties.BuiltFAR
-            var landuseDescription = FAR(parseInt(hoveredFeature.properties.ResidFAR)).description
+            var maxfar = hoveredFeature.properties.ResidFAR
 
             var popupContent = `
               <div>
               <h4>Zoning District:  ${address}</h4> <br/>
                  <p> Built FAR =  ${built}<br/>
-                ${landuseDescription} </p>
+              Max Residential FAR =  ${maxfar} </p>
               </div>
             `
 
